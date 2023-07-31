cbf86f733cff3338275670c02a31918de60d993a
Luke Millam <luke.millam@gmail.com>
2023-07-30 22:27:23 -0700

    m tag sucks

--- pages/app.jsx
+++ pages/app.jsx
@@ -2,13 +2,14 @@ import '../styles/navBar.module.css'
 import Head from 'next/head'
 import NavBar from '../Components/NavBar.jsx'
 
-export default function App({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
     return (
       <>
         <Component {...pageProps} />
         <Head>
           <title>Luke Millam</title>
         </Head>
+        <h1>Luke Millam</h1>
         <div>
           <NavBar />
         </div>
