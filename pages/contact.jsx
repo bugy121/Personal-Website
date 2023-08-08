22ed577742c3633e9150182a889fb277a2bc92b6
Luke Millam <luke.millam@gmail.com>
2023-08-08 07:55:51 -0700

    navbar finished except background color

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,14 +1,15 @@
 import {Box,Flex,Spacer} from '@chakra-ui/react'
-import Image from 'next/image'
+import Image from 'next/Image'
+import Styles from '../styles/contact.module.css'
 
 export default function contact(){
     return(
         <>
-        <Box h='40px'></Box>
-        <Box h='90px' align='center' fontSize='50px' fontWeight='800' className='ContactTitle' >Contact</Box>
-        <Box align = 'center'>
-            <Image src='/images/LukeNoBackground.png' width = '300' height = '400' alt=''/>
+        <Box justify-content='center' alignItems='center'>
+            <Image justify-content='center' className={Styles.backgroundImage} src='/images/LukeNoBackground.png' width='400' height='600' alt=''/>
         </Box>
+        <Box className={Styles.frontText} paddingTop='30px' align='center' fontSize='50px' fontWeight='800' z-inderx='2'>Contact</Box>
+        
         </>
     )
 }
